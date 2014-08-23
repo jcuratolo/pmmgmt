d285ab01015e23f66c1ca363f722cf57b0301627
jcuratolo <jcuratolo@me.com>
2014-08-23 12:32:11 -0700

    Begin project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pmmgmt::Application.config.secret_key_base = 'c11df9a467fec8768a40f6fa3465f7a41d2e565b9d0a0f7e1cdaa28f76fa57181df8d80615975d35b95b5d750df4cc34b1fd83d30e0d3a023bf7039ea58a4ddb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pmmgmt::Application.config.session_store :cookie_store, key: '_pmmgmt_session'
