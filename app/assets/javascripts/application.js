46ee55cdd8efc294152d2dbdb7819a90deb43c69
jzander <jeremy.zander@gmail.com>
2014-08-28 12:37:22 -0700

    updated styling on static index added sample map

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require bootstrap
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
