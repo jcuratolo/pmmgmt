6691cd379817b4c044ce28a8ebfba8760e5d1499
jzander <jeremy.zander@gmail.com>
2014-08-29 14:35:32 -0700

    updated styles/carousel slider/jquery/navbar hover

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap
 //= require_tree .
 
 $(document).ready(function(){
@@ -23,3 +23,7 @@ $(document).ready(function(){
         return false;
     });
 }); 
+
+$('.carousel').carousel({
+    interval: 3000
+});
