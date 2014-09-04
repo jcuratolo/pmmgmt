fa8104d8cb773467afed74c6f404419fd2afe938
jzander <jeremy.zander@gmail.com>
2014-09-04 16:34:01 -0700

    updated image upload

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -10,12 +10,16 @@ class UnitsController < ApplicationController
     @unit = Unit.new
   end
   def create
-    @unit = Unit.new(params.require(:unit).permit(:uNumber, :uType, :uBedroomCount, :uBathroomCount, :uSquareFootage, :uDescription, :building_id))
+    @unit = Unit.new(params.require(:unit).permit(:uNumber, :uType, :uBedroomCount, :uBathroomCount, :uSquareFootage, :uDescription, :unitimage, :building_id))
       if @unit.save
         redirect_to units_path
       else 
         render 'new'
-      end 
+      end
+  end
+
+  def user_params
+    params.require(:user).permit(:unitimage)
   end
 
   def destroy
