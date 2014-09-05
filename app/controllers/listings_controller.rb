cbd5814a659819afb04cc179171a7e5d6796b968
jcuratolo <jcuratolo@me.com>
2014-09-05 15:21:00 -0700

    Removed erroneous table listing_reports

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,10 +1,10 @@
 class ListingsController < ApplicationController
   def index
-      #@listings = ListingReport.find_by_sql("SELECT * FROM buildings " + "INNER JOIN units ON buildings.id = units.building_id " + "WHERE \"uListing\" =true" )
-      #@listings = Unit.all
+      @listings = Listing.find_by_sql("SELECT * FROM buildings " + "INNER JOIN units ON buildings.id = units.building_id " + "WHERE \"uListing\" =true" )
+      @listings = Unit.all
       #@listings = Unit.where(uListing:true)
       @listings = Listing.all
-      respond_with @listings, each_serializer: 
+      respond_with @listings
   end
 
   def show
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140905213200) do
+ActiveRecord::Schema.define(version: 20140905221544) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,12 +36,6 @@ ActiveRecord::Schema.define(version: 20140905213200) do
     t.string   "bUnitCount"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "listing_reports", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "lbAddressLine1"
   end
 
   create_table "listings", force: true do |t|
