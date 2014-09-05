d5a627560de8ac091d5b6ee25e2c7620ba7c827e
jcuratolo <jcuratolo@me.com>
2014-09-04 17:38:51 -0700

    Rspec working again

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -7,4 +7,3 @@ class Unit < ActiveRecord::Base
   has_attached_file :unitimage, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :unitimage, :content_type => /\Aimage\/.*\Z/
 end
-`
