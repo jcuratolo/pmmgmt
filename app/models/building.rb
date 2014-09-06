a07d368b54f2b55962f45339fe0a841fc36bc4b1
jcuratolo <jcuratolo@me.com>
2014-09-06 15:07:05 -0700

    Multiple map pins working in such an ugly way...

--- app/models/building.rb
+++ app/models/building.rb
@@ -3,13 +3,13 @@ class Building < ActiveRecord::Base
   has_many :units
   has_many :building_features
 
-  geocoded_by :bAddress1
+  geocoded_by :full_street_address
   after_validation :geocode
 
   validates :bName, :bAddress1, :bCity, :bState, :bZip, presence: true
   validates :bUnitCount, numericality: true, allow_nil: true
 
   def full_street_address
-    return bAddress1
+    return "#{bAddress1}, #{bCity}, #{bState}"
   end
 end
