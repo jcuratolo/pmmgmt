e138e0312c2e9be81a49615810ec6710a3e46113
jzander <jeremy.zander@gmail.com>
2014-09-07 22:04:20 -0400

    updated static#index with listings view beside map, updated app.js and added js to static#index for smooth scrolling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,25 +27,5 @@ $(document).ready(function () {
       $(this).parent('li').addClass('active')
     });
 
-    $('#togglenav').hide();
-      $('#menubutton').click(function() {
-          $('#togglenav').slideToggle("900");
-          return false;
-      });
 
-    $('.carousel').carousel({
-        interval: 3000
-      });
 
- $(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-    if (target.length) {
-  $('html,body').animate({scrollTop: target.offset().top}, 1000);
-        return false;
-      }
-    }
-  });
-}); 
