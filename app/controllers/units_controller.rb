6739b426dc72d3dd7f5e7a5d7d29d4eb24812734
blaisethomas <blaisegthomas@gmail.com>
2014-09-07 20:34:04 -0700

    buildings index update

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -36,7 +36,7 @@ class UnitsController < ApplicationController
   def update
     @unit = Unit.find(params[:id]) 
       if @unit.update_attributes(params.require(:unit).permit(:uNumber, :uType, :uBedroomCount, :uBathroomCount, :uSquareFootage, :building_id, :uListing, :uDescription, :unitimage, :uPrice))
-      redirect_to unit_path 
+      redirect_to units_path 
       else
       render 'edit' 
       end
