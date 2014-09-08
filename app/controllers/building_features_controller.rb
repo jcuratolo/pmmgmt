6ef05c185f489ae8c5f311487d78d858c73ce0b6
jcuratolo <jcuratolo@me.com>
2014-09-08 01:14:29 -0700

    Fixed add building feature modal

--- app/controllers/building_features_controller.rb
+++ app/controllers/building_features_controller.rb
@@ -5,9 +5,11 @@ class BuildingFeaturesController < ApplicationController
   def show
     @buildings_feature = BuildingFeature.find(params[:id])
   end
+
   def new
     @building_feature = BuildingFeature.new
   end
+
   def create
     @building_feature = BuildingFeature.new(params.require(:building_feature).permit(:bfName, :building_id))
       if @building_feature.save
@@ -16,17 +18,16 @@ class BuildingFeaturesController < ApplicationController
         render 'new'
       end
   end
+
   def destroy
     @building_feature = BuildingFeature.find(params[:id])
       @building_feature.destroy
       redirect_to buildings_path
   end
 
-  
-
-  def edit
+  def update
   end
 
-  def update
+  def edit
   end
 end
